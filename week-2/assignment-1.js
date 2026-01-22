a01dabf171af10b7086828d7181ae7f7fd01a0fa
Nilufar Karimova <nilufar.qa@gmail.com>
2026-01-21 21:51:29 -0500

    test update

---
+++ week-2/assignment-1.js
@@ -0,0 +1,24 @@
+//Arithmetic Operators
+
+//1. Create two numbers and:
+//- Add them
+//- Subtract them
+//- Multiply them
+//- Divide them
+
+//  Print each result.
+
+let number1 = 6;
+let number2 = 8;
+
+// Add them
+console.log(number1 + number2);
+
+// Subtract them
+console.log(number1 - number2);
+
+// Multiply them
+console.log(number1 * number2);
+
+// Divide them
+console.log(number1 / number2);
