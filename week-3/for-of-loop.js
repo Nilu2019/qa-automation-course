db6a7e4fd89244aa45144538feed372d54520ffe
Nilufar Karimova <nilufar.qa@gmail.com>
2026-02-07 12:27:14 -0500

    for of loop practice

---
+++ week-3/for-of-loop.js
@@ -0,0 +1,17 @@
+// for (let i = 1;  i <= 5; i++) {
+//console.log(Count:, i)
+//}
+
+// const colors = ["green", "yellow", "blue"];
+
+// for (const color of colors) {
+//   console.log(color);
+// }
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+
+  console.log(i);
+}
